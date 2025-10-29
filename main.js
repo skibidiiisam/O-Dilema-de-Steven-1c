959ef8f29516259cfc4cddb63e65822f30c86ad7
skibidiiisam <sophia.bello@escola.pr.gov.br>
2025-10-29 10:17:36 -0300

    gato

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(avanca)
 
 avanca.forEach(button => {
 button.addEventListener('click',function () {
-const atual = documen.querySelector('.ativo')
+const atual = document.querySelector('.ativo')
 const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
 
 atual.ClassList.remove('ativo')
