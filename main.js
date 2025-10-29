5d269af553e3dfd1f73b137c231e78c590395809
skibidiiisam <sophia.bello@escola.pr.gov.br>
2025-10-29 10:17:03 -0300

    n

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const avanca =document.querySelectorAll('.btn-proximo')
 console.log(avanca)
 
 avanca.forEach(button => {
-buttton.addEventListener('click',function () {
+button.addEventListener('click',function () {
 const atual = documen.querySelector('.ativo')
 const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
 
